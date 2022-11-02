ef61fdac4ebd7d8e268ec3ffce61e5af7c28ef91
Priyangsu Banerjee <priyangsu26@gmail.com>
2022-11-02 20:45:40 +0530

    start

--- index.js
+++ index.js
@@ -4,9 +4,15 @@ dotenv.config();
 
 const bot = new Composer();
 
-bot.on("message", (msg) => {
-  console.log(msg);
-  msg.reply("Hello World");
+bot.on("message", (context) => {
+  const message = context.message.text;
+  const chatId = context.message.chat.id;
+
+  if (message === "/start") {
+    context.reply(
+      "Hello, welcome to the @ready_money_bot we are here to help you track your expenses and income"
+    );
+  }
 });
 
 module.exports = bot;
