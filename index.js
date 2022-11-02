791e3aaa9b1e4fbdeeb2d48c8153e138fde7bf4e
Priyangsu Banerjee <priyangsu26@gmail.com>
2022-11-02 21:17:55 +0530

    env

--- index.js
+++ index.js
@@ -1,18 +1,24 @@
 const { Composer } = require("micro-bot");
+const { Telegraf } = require("telegraf");
 const dotenv = require("dotenv");
 dotenv.config();
 
 const bot = new Composer();
 
-bot.on("message", (context) => {
+//const bot = new Telegraf(process.env.TOKEN);
+
+bot.on("message", async (context) => {
   const message = context.message.text;
-  const chatId = context.message.chat.id;
 
   if (message === "/start") {
-    context.reply(
-      "Hello, welcome to the @ready_money_bot we are here to help you track your expenses and income"
+    await context.reply(
+      "Hello, welcome to the @ready_money_bot we are here to track your expenses and income and help you save money. Before we start, we need to create a new account for you."
     );
+  } else {
+    context.reply("Please enter your name");
   }
 });
 
 module.exports = bot;
+
+//bot.launch();
